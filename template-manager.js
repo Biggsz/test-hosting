6187dff8a1b3432da869672d7e49e33cb6283a15
Biggsz <gilles.verga@gmail.com>
2021-11-17 10:18:07 +0100

    Update template-manager.js

--- template-manager.js
+++ template-manager.js
@@ -2,9 +2,11 @@ var callback = function(){
  let xhttp = new XMLHttpRequest();
  xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        let div = document.createElement("div");
-        div.innerHTML = this.responseText;
-        document.body.prepend(div.querySelector('.header'));
+     let div = document.createElement("div");
+     div.innerHTML = this.responseText;
+     document.body.prepend(div.querySelector('.header'));
+     let content = document.body.querySelector('.content');
+     content.className = content.className + ' visible';
     }
 };
 xhttp.open("GET", "header.html", true);
